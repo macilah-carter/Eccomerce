7c4533f140e970256cca7ff680b19ed7cf0420ef
macilah-carter <museemacilah@gmail.com>
2024-09-23 11:06:33 +0300

    uri

--- backend/Database/DB/db.js
+++ backend/Database/DB/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const DataBase = async () => {
     try {
+        const conString = process.env.DATABASE_URL || `mongodb+srv://${process.env.USERNAME}:${process.env.DB_PASS}@website.2viiaas.mongodb.net/eccomerce`
         const connection = await mongoose.connect(process.env.DATABASE_URL)
         console.log('Database connected')
     } catch (error) {
